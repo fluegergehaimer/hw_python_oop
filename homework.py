917e59c62b06d62ebef97dfbd5a7f6b38f146094
Никита Чередниченко <matador4.0@mail.ru>
2023-09-03 21:40:42 +0300

    First working commit

--- homework.py
+++ homework.py
@@ -1,7 +1,12 @@
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-
-    def _init__(self, training_type: str, duration: float, distance: float, speed: float, calories: float):
+    def __init__(self,
+                 training_type: str,
+                 duration: float,
+                 distance: float,
+                 speed: float,
+                 calories: float
+                 ) -> None:
         self.training_type = training_type
         self.duration = duration
         self.distance = distance
@@ -9,7 +14,7 @@ class InfoMessage:
         self.calories = calories
 
     def get_message(self) -> str:
-        return (f'Тип тренировки: {training.__class__.__name__}; '
+        return (f'Тип тренировки: {self.training_type}; '
                 f'Длительность: {self.duration:.3f} ч.; '
                 f'Дистанция: {self.distance:.3f} км; '
                 f'Ср. скорость: {self.speed:.3f} км/ч; '
@@ -30,19 +35,14 @@ class Training:
         self.action = action
         self.duration = duration
         self.weight = weight
-        self.distance = self.get_distance()
-        self.speed = self.get_mean_speed()
-        self.calories = self.get_spent_calories()
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
-        distance = self.action * self.LEN_STEP / self.M_IN_KM
-        return distance
+        return self.action * self.LEN_STEP / self.M_IN_KM
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-
-        return Training.get_distance(self) / self.duration
+        return self.get_distance() / self.duration
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
@@ -50,41 +50,53 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        
-        return InfoMessage.get_message(self)
+
+        return InfoMessage(self.__class__.__name__,
+                           self.duration, self.get_distance(),
+                           self.get_mean_speed(),
+                           self.get_spent_calories()
+                           )
+
 
 class Running(Training):
     """Тренировка: бег."""
     CALORIES_MEAN_SPEED_MULTIPLIER = 18
     CALORIES_MEAN_SPEED_SHIFT = 1.79
 
-    def __init__(self, action: int, duration:
-                 float, weight: float) -> None:
+    def __init__(self, action: int,
+                 duration: float,
+                 weight: float
+                 ) -> None:
         super().__init__(action, duration, weight)
 
-
     def get_spent_calories(self) -> float:
-        return ((self.CALORIES_MEAN_SPEED_MULTIPLIER
-                * self.get_mean_speed() + self.CALORIES_MEAN_SPEED_SHIFT)
-                * self.weight / self.M_IN_KM * self.DURATION_MIN)
+        return ((self.CALORIES_MEAN_SPEED_MULTIPLIER * self.get_mean_speed()
+                 + self.CALORIES_MEAN_SPEED_SHIFT) * self.weight / self.M_IN_KM
+                * (self.duration * self.DURATION_MIN))
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    K_1 = 0.035
-    K_2 = 0.029
+    K_1: float = 0.035
+    K_2: float = 0.029
+    METERS_PER_SECOND = 0.278
+    SM_IN_M = 100
 
-    def __init__(self, action: int, duration:
-                 float, weight: float, height: float) -> None:
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float,
+                 height: float
+                 ) -> None:
         super().__init__(action, duration, weight)
         self.height = height
-        self.distance = super().get_distance()
-        self.speed = super().get_mean_speed
 
     def get_spent_calories(self) -> float:
-        return (((self.K_1 * self.weight
-                  + (self.get_mean_speed()**2 / self.height) * self.K_2
-                  * self.weight) * (self.duration * self.DURATION_MIN)))
+        return ((self.K_1 * self.weight
+                + ((super().get_mean_speed() * self.METERS_PER_SECOND)**2
+                   / (self.height / self.SM_IN_M))
+                 * self.K_2 * self.weight)
+                * (self.duration * self.DURATION_MIN))
 
 
 class Swimming(Training):
@@ -97,20 +109,20 @@ class Swimming(Training):
                  action: int,
                  duration: float,
                  weight: float,
-                 length_pool: float,
-                 count_pool: float
+                 length_pool: int,
+                 count_pool: int
                  ) -> None:
-        super().__init__(self, action, duration, weight)
+        super().__init__(action, duration, weight)
         self.length_pool = length_pool
         self.count_pool = count_pool
 
     def get_mean_speed(self) -> float:
-        return (self.length_pool * self.count_pool
-                / self.M_IN_KM / (self.duration * self.DURATION_MIN))
+        return ((self.length_pool * self.count_pool)
+                / self.M_IN_KM / self.duration)
 
     def get_spent_calories(self) -> float:
-        return ((self.get_mean_speed() + self.K_1) * self.K_2 * self.weight
-                * self.duration * self.DURATION_MIN)
+        return ((self.get_mean_speed() + self.K_1)
+                * self.K_2 * self.weight * self.duration)
 
 
 def read_package(workout_type: str, data: list) -> Training:
@@ -120,7 +132,7 @@ def read_package(workout_type: str, data: list) -> Training:
                       'WLK': SportsWalking,
                       'SWM': Swimming
                       }
-    
+
     return TRAINING_TYPES[workout_type](*data)
 
 
@@ -133,8 +145,8 @@ def main(training: Training) -> None:
 
 if __name__ == '__main__':
     packages = [
-        #('SWM', [720, 1, 80, 25, 40]),
-       # ('RUN', [15000, 1, 75]),
+        ('SWM', [720, 1, 80, 25, 40]),
+        ('RUN', [15000, 1, 75]),
         ('WLK', [9000, 1, 75, 180]),
     ]
 
